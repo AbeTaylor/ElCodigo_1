eeee0727732738e06a2b0f97e721cb3851961053
Abe Taylor <ataylo05@gmail.com>
2015-08-13 11:56:20 -0400

    initial Code check-in

---
+++ src/ElCodigo/CustomButtons/Favorites/AddToFavorites.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Security.Accounts;
+using Sitecore.Shell;
+using Sitecore.Shell.Framework.Commands;
+
+namespace ElCodigo.CustomButtons.Favorites
+{
+    public class AddToFavorites : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            if (context == null || context.Items == null || context.Items.Length == 0)
+            {
+                return;
+            }
+            var item = context.Items[0];
+
+            var favorites = UserOptions.Favorites.Root;
+
+            User user = Sitecore.Context.User;
+            user.Profile.SetCustomProperty("UserFavorite", item.ID.ToString());
+            user.Profile.Save();
+        }
+    }
+}
---
+++ src/ElCodigo/CustomButtons/Favorites/GoToFavorite.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Security.Accounts;
+using Sitecore.Shell.Framework.Commands;
+using System;
+
+namespace ElCodigo.CustomButtons.Favorites
+{
+    public class GoToFavorite : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            if (context == null || context.Items == null || context.Items.Length == 0)
+            {
+                return;
+            }
+            var item = context.Items[0];
+
+            User user = Sitecore.Context.User;
+            Item myItem = item.Database.GetItem(new ID(user.Profile.GetCustomProperty("UserFavorite")));
+            string load = String.Concat(new object[] { "item:load(id=", myItem.ID, ",language=", myItem.Language, ",version=", myItem.Version, ")" });
+            Sitecore.Context.ClientPage.SendMessage(this, load);
+        }
+    }
+}
