bf6eda40c4445def5c130adcbd1506112b1d0c57
Abe Taylor <ataylo05@gmail.com>
2015-08-13 12:24:52 -0400

    Added Comments to code

--- src/ElCodigo/CustomButtons/Favorites/AddToFavorites.cs
+++ src/ElCodigo/CustomButtons/Favorites/AddToFavorites.cs
@@ -9,16 +9,25 @@ namespace ElCodigo.CustomButtons.Favorites
 {
     public class AddToFavorites : Command
     {
+        /// <summary>
+        /// Method called on click of the command (MUST EXIST if inheriting from command)
+        /// </summary>
+        /// <param name="context">Context that the command is running on</param>
         public override void Execute(CommandContext context)
         {
+            //Make sure the context isnt null, and that there are context items
             if (context == null || context.Items == null || context.Items.Length == 0)
             {
                 return;
             }
+
+            //Get the context Item
             var item = context.Items[0];
 
+            //Ignore for now
             var favorites = UserOptions.Favorites.Root;
 
+            //Get User, Set favorite item, save user profile
             User user = Sitecore.Context.User;
             user.Profile.SetCustomProperty("UserFavorite", item.ID.ToString());
             user.Profile.Save();
--- src/ElCodigo/CustomButtons/Favorites/GoToFavorite.cs
+++ src/ElCodigo/CustomButtons/Favorites/GoToFavorite.cs
@@ -9,14 +9,22 @@ namespace ElCodigo.CustomButtons.Favorites
 {
     public class GoToFavorite : Command
     {
+        /// <summary>
+        /// Method called on click of the command (MUST EXIST if inheriting from command)
+        /// </summary>
+        /// <param name="context">Context that the command is running on</param>
         public override void Execute(CommandContext context)
         {
+            //Make sure the context isnt null, and that there are context items
             if (context == null || context.Items == null || context.Items.Length == 0)
             {
                 return;
             }
+
+            //Get the context Item
             var item = context.Items[0];
 
+            //Get User, get favorite item from the DB, create load string, send load message to UI
             User user = Sitecore.Context.User;
             Item myItem = item.Database.GetItem(new ID(user.Profile.GetCustomProperty("UserFavorite")));
             string load = String.Concat(new object[] { "item:load(id=", myItem.ID, ",language=", myItem.Language, ",version=", myItem.Version, ")" });
