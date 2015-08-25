1dfa53237f49a126f039204b50482cb729d5eec2
Will Sharp <willwsharp@gmail.com>
2015-08-25 16:58:51 -0400

    add to favorites container functional

--- src/ElCodigo/CustomButtons/Favorites/AddToFavorites.cs
+++ src/ElCodigo/CustomButtons/Favorites/AddToFavorites.cs
@@ -1,6 +1,7 @@
 ﻿using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
+using Sitecore.Pipelines.InsertRenderings.Processors;
 using Sitecore.Security.Accounts;
 using Sitecore.Shell;
 using Sitecore.Shell.Framework.Commands;
@@ -23,14 +24,19 @@ namespace ElCodigo.CustomButtons.Favorites
 
             //Get the context Item
             var item = context.Items[0];
+            var FavoriteContainer = item.Database.GetItem("{871D33CC-00C7-4CAA-A293-6D81B0D2C0AD}");
+            var newFavoriteItem = FavoriteContainer.Add(item.Name, new TemplateID(new ID("{F52BCA1A-0A34-46DC-AE5F-B3BB97389F75}")));
+            newFavoriteItem.Editing.BeginEdit();
+            newFavoriteItem.Fields["FavoriteItem"].Value = item.ID.ToString();
+            newFavoriteItem.Editing.EndEdit();
 
             //Ignore for now
-            var favorites = UserOptions.Favorites.Root;
+            //var favorites = UserOptions.Favorites.Root;
 
-            //Get User, Set favorite item, save user profile
-            User user = Sitecore.Context.User;
-            user.Profile.SetCustomProperty("UserFavorite", item.ID.ToString());
-            user.Profile.Save();
+            ////Get User, Set favorite item, save user profile
+            //User user = Sitecore.Context.User;
+            //user.Profile.SetCustomProperty("UserFavorite", item.ID.ToString());
+            //user.Profile.Save();
         }
     }
 }
