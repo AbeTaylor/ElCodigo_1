ae1f658e1743d46b00916d3ea0dd40e058271c97
Abe Taylor <ataylo05@gmail.com>
2015-09-17 10:54:01 -0400

    code refactor

--- src/ElCodigo/CustomButtons/Editor/ContentEditor.cs
+++ src/ElCodigo/CustomButtons/Editor/ContentEditor.cs
@@ -2,12 +2,7 @@
 using Sitecore.Shell.Applications.ContentManager;
 using Sitecore.Web.UI.Sheer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Sitecore.Web.UI.HtmlControls;
 using Sitecore.Data.Items;
-using Sitecore.Data;
 using Sitecore.Globalization;
 
 namespace ElCodigo.CustomButtons.Editor
